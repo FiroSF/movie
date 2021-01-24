4c17a2cc9f624adbed2e24672ecd2d1a2c33e8cb
Firo_SF <46997936+FiroSF@users.noreply.github.com>
2021-01-24 10:11:15 +0900

    3.2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,19 @@
 module.exports = {
-    "env": {
-        browser: true,
-        es6: true,
-        jest: true,
-        node: true
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
 };
